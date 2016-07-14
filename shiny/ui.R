8c6fe3fd108b8a8f8655fecd93cb68b400a04e76
Andrey Kotov <hokum@dived.me>
2016-07-14 22:34:02 +0400

    typos

--- shiny/ui.R
+++ shiny/ui.R
@@ -13,7 +13,7 @@ shinyUI(fluidPage(
     sidebarLayout(
         
         sidebarPanel(
-            textInput('userQuery', 'Say something?',value = ''),
+            textInput('userQuery', 'Say something?)',value = ''),
             sliderInput('thresholdPercents', label='Freq.threshold', min = 1, max=100,value = 90)
         ),
         
@@ -29,10 +29,10 @@ shinyUI(fluidPage(
                         tabPanel("Readme", 
                                  h4('How it works?'),
                                  p('We have a lot of words and phrases: this is a our text corpus.'),
-                                 p('Then we do processing and prepared 3 tables: most common phrases from our text, containing 2,3 and 4 words. This means that we have collected N-grams, also we know the frequency of these phrases.'),
+                                 p('Then we did processing and prepared 3 tables: most common phrases from our corpus, containing 2,3 and 4 words. This means that we have collected N-grams with frequency of these phrases.'),
                                  p('Idea how to predict next word: let\'s take last 3 words in the sentence and check if it is first 3 words of 4-gram?  No? Then, take last 2 words and check for 3-gram. If not, may be last word is a part of 2-gram?'),
-                                 p('So, we found some N-grams starting with last words of your sentence and now have to choose one. You can set threshold parameter: if it equals to 100 algorythm picks most frequent word, in other case - one random word with frequency greater than X percent.'),
-                                 p('It works! Just try to write some words and surprise with prediction'),
+                                 p('So, we found some N-grams starting with last words of your sentence and now have to choose one. You can set threshold level: if it equals to 100%, algorythm picks most frequent word, in other case - random word with frequency greater than X percent of most frequent.'),
+                                 p('It works! Just try to write some words :)'),
                                  
                                  a(href='https://github.com/hokumski/capstone-project-datascience','"Word Prediction with N-Grams" on GitHub'),
                                  br(),
